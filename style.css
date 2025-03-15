092d5cd3e147d9d38f2e57c7f1a84ff15f7bdeee
wll16 <002016casalsinho@gmail.com>
2025-03-15 12:52:48 -0300

    Update style.css 01

--- style.css
+++ style.css
@@ -1,1 +1,16 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin: 50px;
+}
 
+input {
+    width: 50px;
+    padding: 5px;
+}
+
+button {
+    padding: 8px 12px;
+    margin-top: 10px;
+    cursor: pointer;
+}
